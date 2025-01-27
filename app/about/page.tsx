465604abf441c4349e8e83930cb68f2211703a55
Noah B <152567394+SolutionsAsService@users.noreply.github.com>
2025-01-26 17:25:56 -0800

    Create page.tsx

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+export default function About() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-4">About Us</h1>
+      <p>
+        Rafting Adventures has been crafting world-class rafting experiences for over
+        a decade. Our mission is to connect people with nature through the thrill of
+        white-water rafting.
+      </p>
+    </div>
+  );
+}
